099281e747707aa850a3177d60fa1b42e435a207
Sumit Singh <sumitsinghbais@gmail.com>
2026-03-10 22:57:52 +0530

    Initial commit - ecommerce project

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,242 @@
+// icons
+import arrow from './arrow.png'
+// import breadcrumb_arrow from './breadcrumb_arrow.png'
+import breadcrumb_arrow from './breadcrum_arrow.png'
+import cart_cross_icon from './cart_cross_icon.png'
+import cart_icon from './cart_icon.png'
+import cart_product_icon from './cart_product_icon.png'
+import dropdown_icon from './dropdown_icon.png'
+import hand_icon from './hand_icon.png'
+import instagram_icon from './instagram_icon.png'
+import nav_dropdown from './nav_dropdown.png'
+// import pinterest_icon from './pinterest_icon.png'
+import pintester_icon from './pintester_icon.png'
+import star_dull_icon from './star_dull_icon.png'
+import star_icon from './star_icon.png'
+import whatsapp_icon from './whatsapp_icon.png'
+
+// logos
+import logo from './logo.png'
+import logo_big from './logo_big.png'
+
+//icons
+import search_icon from './search_icon.png'
+import profile_icon from './profile_icon.png'
+import menu_icon from './menu_icon.png'
+
+// banners
+import banner_kids from './banner_kids.png'
+import banner_mens from './banner_mens.png'
+import banner_women from './banner_women.png'
+
+// hero images
+import hero_image from './hero_image.png'
+import exclusive_image from './exclusive_image.png'
+
+// product images
+import p1_product from './p1_product.png'
+import p1_product_i1 from './p1_product_i1.png'
+import p1_product_i2 from './p1_product_i2.png'
+import p1_product_i3 from './p1_product_i3.png'
+import p1_product_i4 from './p1_product_i4.png'
+
+import product_1 from './product_1.png'
+import product_2 from './product_2.png'
+import product_3 from './product_3.png'
+import product_4 from './product_4.png'
+import product_5 from './product_5.png'
+import product_6 from './product_6.png'
+import product_7 from './product_7.png'
+import product_8 from './product_8.png'
+import product_9 from './product_9.png'
+import product_10 from './product_10.png'
+import product_11 from './product_11.png'
+import product_12 from './product_12.png'
+import product_13 from './product_13.png'
+import product_14 from './product_14.png'
+import product_15 from './product_15.png'
+import product_16 from './product_16.png'
+import product_17 from './product_17.png'
+import product_18 from './product_18.png'
+import product_19 from './product_19.png'
+import product_20 from './product_20.png'
+import product_21 from './product_21.png'
+import product_22 from './product_22.png'
+import product_23 from './product_23.png'
+import product_24 from './product_24.png'
+import product_25 from './product_25.png'
+import product_26 from './product_26.png'
+import product_27 from './product_27.png'
+import product_28 from './product_28.png'
+import product_29 from './product_29.png'
+import product_30 from './product_30.png'
+import product_31 from './product_31.png'
+import product_32 from './product_32.png'
+import product_33 from './product_33.png'
+import product_34 from './product_34.png'
+import product_35 from './product_35.png'
+import product_36 from './product_36.png'
+
+
+// assets export
+export const assets = {
+  arrow,
+  search_icon,
+  profile_icon,
+  breadcrumb_arrow,
+  cart_cross_icon,
+  cart_icon,
+  cart_product_icon,
+  dropdown_icon,
+  hand_icon,
+  instagram_icon,
+  nav_dropdown,
+  pintester_icon,
+  star_dull_icon,
+  star_icon,
+  whatsapp_icon,
+  logo,
+  logo_big,
+  banner_kids,
+  banner_mens,
+  banner_women,
+  hero_image,
+  exclusive_image,
+  menu_icon
+}
+
+
+// products data
+export const products = [
+
+{
+_id:"aaaaa",
+name:"Women Round Neck Cotton Top",
+description:"A lightweight, usually knitted, pullover shirt",
+price:100,
+image:[p1_product,p1_product_i1,p1_product_i2,p1_product_i3,p1_product_i4],
+category:"Women",
+subCategory:"Topwear",
+sizes:["S","M","L"],
+date:1716634345448,
+bestseller:true
+},
+
+{
+_id:"aaaab",
+name:"Women Casual Pink Top",
+description:"Stylish cotton casual top",
+price:120,
+image:[product_1],
+category:"Women",
+subCategory:"Topwear",
+sizes:["S","M","L"],
+date:1716621345448,
+bestseller:true
+},
+
+{
+_id:"aaaac",
+name:"Women Fashion Top",
+description:"Trendy modern fashion top",
+price:140,
+image:[product_2],
+category:"Women",
+subCategory:"Topwear",
+sizes:["S","M","L"],
+date:1716621345448,
+bestseller:false
+},
+
+{
+_id:"aaaad",
+name:"Women Stylish Top",
+description:"Elegant stylish wear",
+price:150,
+image:[product_3],
+category:"Women",
+subCategory:"Topwear",
+sizes:["S","M","L"],
+date:1716621345448,
+bestseller:true
+},
+
+{
+_id:"aaaae",
+name:"Women Winter Top",
+description:"Comfortable winter wear",
+price:200,
+image:[product_4],
+category:"Women",
+subCategory:"Winterwear",
+sizes:["S","M","L"],
+date:1716621345448,
+bestseller:false
+},
+
+{
+_id:"aaaaf",
+name:"Women Sweater",
+description:"Warm wool sweater",
+price:220,
+image:[product_5],
+category:"Women",
+subCategory:"Winterwear",
+sizes:["S","M","L"],
+date:1716621345448,
+bestseller:true
+},
+
+{
+_id:"aaaag",
+name:"Women White Sweater",
+description:"Soft wool sweater",
+price:240,
+image:[product_6],
+category:"Women",
+subCategory:"Winterwear",
+sizes:["S","M","L"],
+date:1716621345448,
+bestseller:false
+},
+
+{
+_id:"aaaah",
+name:"Women Printed Dress",
+description:"Stylish printed dress",
+price:260,
+image:[product_7],
+category:"Women",
+subCategory:"Dress",
+sizes:["S","M","L"],
+date:1716621345448,
+bestseller:true
+},
+
+{
+_id:"aaaai",
+name:"Women Party Dress",
+description:"Elegant party dress",
+price:300,
+image:[product_8],
+category:"Women",
+subCategory:"Dress",
+sizes:["S","M","L"],
+date:1716621345448,
+bestseller:true
+},
+
+{
+_id:"aaaaj",
+name:"Women Casual Shirt",
+description:"Lightweight casual shirt",
+price:180,
+image:[product_9],
+category:"Women",
+subCategory:"Topwear",
+sizes:["S","M","L"],
+date:1716621345448,
+bestseller:false
+}
+
+]
